22c7a83b69e1a214d6a419845f01fb8dcf4303ea
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-03-22 22:17:28 -0400

    slider+year based filter (only removal) implemented

--- static/js/vis_map.js
+++ static/js/vis_map.js
@@ -1,8 +1,52 @@
 function vis_map(data){
-  var width = Math.max(960, window.innerWidth*0.8),
+
+  //Analyze input dataset to set proper limits/scales on the visualization
+  var colors = d3.scale.category20()
+  var categories = {}
+
+  $.each(data,function(index,v){
+    if ( !(v.category in categories) ) {
+      categories[v.category] = {color: colors(Object.keys(categories).length)}
+    }
+  })
+
+  var daterange = [d3.min(data,function(d){return d.date}),d3.max(data, function(d){return d.date})]
+
+  var rscale = d3.scale.linear()
+    .domain([d3.min(data,function(d){return d.duration}),d3.max(data, function(d){return d.duration})])
+    .range([1500,5500])
+    .nice();
+  //
+
+  //jquery-ui slider setup
+  $( "#year-slider" ).slider({
+    range: true,
+    min: daterange[0],
+    max: daterange[1],
+    values: daterange,
+    change: function (event, ui ){
+      var newVisData = []
+      $.each(data,function(index,v) {
+        var n = [ui.values[0],ui.values[1]]
+        if (v.date >= n[0] && v.date <= n[1]) {
+          newVisData.push(v)
+        }
+      })
+      updateVis(newVisData)
+    },
+    slide: function( event, ui ) {
+      $( "#slider-label-text" ).val( ui.values.join(' - ') );
+    }
+  });
+    //Set the initial values outside of change, so we don't have to put null checking logic in the event handler
+  $( "#slider-label-text" ).val( daterange.join(' - '))
+  //
+
+
+  var width = 256*5, //5 tiles of 256 pixels each.
+      //width = Math.max(960, window.innerWidth*0.8),
       height = Math.max(500, window.innerHeight);
 
-  width = 256*5;
   var tile = d3.geo.tile()
       .size([width, height]);
 
@@ -43,17 +87,16 @@ function vis_map(data){
         .attr("class","bubble")
 
   var circles = bubbles.append("circle")
-      .attr("fill",'blue')
+      .attr("fill",function(d) {return categories[d.category].color})
         
-  var text = bubbles.append("text")
-       .attr("text-anchor","middle")
-       //.attr("transform", 'translate(0,-18)')
-       .attr('fill','red')
-       .attr('font-size',10)
-       .text(function(d) {
-         return d.name;
-       });
-
+  // var text = bubbles.append("text")
+  //      .attr("text-anchor","middle")
+  //      //.attr("transform", 'translate(0,-18)')
+  //      .attr('fill','red')
+  //      .attr('font-size',10)
+  //      .text(function(d) {
+  //        return d.name;
+  //      });
 
   svg.call(zoom)
   zoomed()
@@ -68,10 +111,10 @@ function vis_map(data){
      .attr("transform",function(d){return "translate("+zoom.translate()+")scale("+zoom.scale()+")"})
           
     circles
-      .attr('r', 50/zoom.scale())
+      .attr('r', function(d) { return Math.sqrt(rscale(d.duration))/zoom.scale()})
 
-    text
-      .attr('font-size',100/zoom.scale())
+    // text
+    //   .attr('font-size',100/zoom.scale())
 
     var image = raster
         .attr("transform", "scale(" + tiles.scale + ")translate(" + tiles.translate + ")")
@@ -88,5 +131,29 @@ function vis_map(data){
         .attr("height", 1)
         .attr("x", function(d) { return d[0]; })
         .attr("y", function(d) { return d[1]; });
+  }
+
+  function updateVis(newVisData) {
+
+
+
+    var b = bubbleG.selectAll('g.bubble')
+      .data(newVisData)
+      .exit()
+      .remove()
+    // b.enter()
+    //   .append("g")
+    //     .attr("transform",function(d){return "translate("+projection([d.lng,d.lat])+")scale("+projection.scale()+")"})
+    //     .attr("class","bubble")
+
+    // b
+    //   .exit()
+    //   .remove()
+
+    // b.append("circle")
+    //     .transition()
+    //     .duration(750)
+    //     .attr('r', function(d) { return Math.sqrt(rscale(d.duration))/zoom.scale()})
+    //     .attr("fill",function(d) {return categories[d.category].color})
   }
 }
