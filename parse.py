16bb1ef37f3d99208756efe44af364f2b3202df0
Vlad Sudilovsky <vsudilovsky@gmail.com>
2014-04-15 18:45:21 -0400

    Slight geocode strategy re-write

--- parse.py
+++ parse.py
@@ -3,42 +3,49 @@ import datetime
 from geopy.geocoders import GoogleV3
 import types
 import time
+import random
 
 YEAR_RANGE = (2000,datetime.datetime.now().year)
 
 def googleGeocode(loc):
-  time.sleep(35)
+  time.sleep(random.Random().random()*5)
   geolocator = GoogleV3()
-  res =  geolocator.geocode(loc)
-  if isinstance(res,types.NoneType):
-    print "Error decoding %s" % loc
-    return None, None
-  (lat, lng) = res[1]
+  try:
+    res =  geolocator.geocode(loc)
+    (lat, lng) = res[1]
+  except:
+    lat,lng = "exception", "exception"
   return lat,lng
 
 def main():
+  s = time.time()
   y = YEAR_RANGE[0]
   results = []
-  while y <= YEAR_RANGE[1]:
-    with open('%s.html' % y) as fp:
-      raw = fp.read()
-      raw = raw.replace('\\r\\n','').replace('\r','').replace('\n','')
-    j = json.loads(raw,encoding='ISO-8859-1') if raw else []
-    for m in j:
-      lat,lng = googleGeocode(m['location'])
-      d = {
-        'bibcode':  m['bibCode'],
-        'start':    datetime.datetime.strptime(m['start'],'%Y-%m-%d'),
-        'end':      datetime.datetime.strptime(m['end'],'%Y-%m-%d'),
-        'lat':      lat,
-        'lng':      lng,
-        'name':     m['title'],
-      }
-      results.append(d)
-    y+=1
+  with open('data/results.json','a') as fp_res:
+    while y <= YEAR_RANGE[1]:
+      with open('%s.html' % y) as fp:
+        print "reading html"
+        raw = fp.read()
+        raw = raw.replace('\\r\\n','').replace('\r','').replace('\n','')
+        j = json.loads(raw,encoding='ISO-8859-1') if raw else []
+        for m in j:
+          print "Working on (dt=%0.2f hours)" % ((time.time()-s)/60/60)
+          print m
+          L = m['location'] if m['location'] else m['address']
+          lat,lng = googleGeocode(L)
+          print lat,lng
+          print "-----------"
+          d = {
+            'bibcode':  m['bibCode'],
+            'start':    datetime.datetime.strptime(m['start'],'%Y-%m-%d').isoformat(),
+            'end':      datetime.datetime.strptime(m['end'],'%Y-%m-%d').isoformat(),
+            'lat':      lat,
+            'lng':      lng,
+            'name':     m['title'],
+          }
+          fp_res.write(json.dumps(d))
+      y+=1
 
-  with open('data/results.json','w') as fp:
-    fp.write(json.dumps(results))
 
 if __name__ == '__main__':
   main()
