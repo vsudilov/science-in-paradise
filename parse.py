a91fb4d67de12d7deaa8d3658948986b26e021d9
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-04-17 20:16:05 -0400

    Add update/insert strategy to sqlite3 db; cycle through 3 geocoding providers before returning failure

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 import os
 import json
 import datetime
-from geopy.geocoders import GoogleV3
+from geopy.geocoders import *
 import types
 import time
 import random
@@ -12,12 +12,15 @@ SQL_CREATE = '''
 CREATE TABLE conferences (id INTEGER PRIMARY KEY AUTOINCREMENT, bibcode, name, start, duration, keywords);
 CREATE TABLE geocode (id INTEGER PRIMARY KEY AUTOINCREMENT, bibcode, name, lat, lng);
 '''
-SQL_SELECT = '''
+SQL_SELECT_GEOCODE = '''
 SELECT lat,lng FROM geocode WHERE bibcode="%s" AND name="%s";
 '''
 SQL_INSERT = '''
 INSERT INTO conferences (bibcode, name, start, duration, keywords) VALUES ("%s","%s","%s","%s","%s");
 INSERT INTO geocode (bibcode, name, lat, lng) VALUES ("%s","%s","%s","%s");
+'''
+SQL_UPDATE = '''
+UPDATE geocode SET lat="%s", lng="%s" WHERE name="%s" AND bibcode="%s"
 '''
 DB_NAME = "conferences.sqlite3"
 SLEEP_GAUSSIAN_PARAMS = {
@@ -28,17 +31,22 @@ SLEEP_GAUSSIAN_PARAMS = {
 
 def googleGeocode(loc):
   time.sleep(abs(random.Random().gauss(SLEEP_GAUSSIAN_PARAMS['mean'],SLEEP_GAUSSIAN_PARAMS['std'])))
-  geolocator = GoogleV3()
-  try:
-    res =  geolocator.geocode(loc)
-  except:
-    return "GeocodeException", "GeocodeException"
-  if res:  
-    lat = res[0]
-    lng = res[1]
-  else:
-    lat,lng = None,None
-  return lat,lng
+  geolocators = [GoogleV3(), OpenMapQuest(), Nominatim()]
+  for geolocator in geolocators:
+    try:
+      res =  geolocator.geocode(loc)
+    except:
+      lat,lng = None, None
+    if res:
+      lat = res[1][0]
+      lng = res[1][1]
+      try:      
+        float(lat)
+        float(lng)
+        return lat,lng
+      except:
+        continue
+  return "failure", "failure"
 
 def init_db(name):
   if not os.path.isfile(name):
@@ -61,31 +69,35 @@ def main():
       raw = raw.replace('\\r\\n','').replace('\r','').replace('\n','')
       j = json.loads(raw,encoding='ISO-8859-1') if raw else []
       for m in j:
-        print "Parsing and Geocoding %s (dt=%0.2f hours)" % (m['title'],(time.time()-s)/60/60)
-        res = db.execute(SQL_SELECT % (m['bibCode'],m['title'].replace('"',''))).fetchall()
         L = m['location'] if m['location'] else m['address']
+        bibcode = m['bibCode']
+        start = datetime.datetime.strptime(m['start'],'%Y-%m-%d').isoformat()
+        duration = (datetime.datetime.strptime(m['end'],'%Y-%m-%d')-datetime.datetime.strptime(m['start'],'%Y-%m-%d')).days
+        name = m['title'].replace('"','')
+        keywords = m['keywords']
+        print "Parsing and Geocoding %s (dt=%0.2f hours)" % (m['title'],(time.time()-s)/60/60)
+        res = db.execute(SQL_SELECT_GEOCODE % (m['bibCode'],m['title'].replace('"',''))).fetchall()
+        strategy = "insert"
         if res:
           lat = res[0][0]
           lng = res[0][1]
           try:
             lat = float(lat)
             lng = float(lng)
+            strategy = "skip"
           except:
-            pass
-          if isinstance(lat,float) and isinstance(lng,float):
-            print "...Skipping since we already seem to have a valid lat/lng"
-            continue
-          else:
-            print "Re-running geocoding of %s" % (m['location'])
+            strategy = "update"
+        if strategy == "skip":
+          continue
         lat,lng = googleGeocode(L)
-        print "Google geocode returned lat,lng [%s,%s] for [%s]" % (lat,lng,L)
-        bibcode = m['bibCode']
-        start = datetime.datetime.strptime(m['start'],'%Y-%m-%d').isoformat()
-        duration = (datetime.datetime.strptime(m['end'],'%Y-%m-%d')-datetime.datetime.strptime(m['start'],'%Y-%m-%d')).days
-        name = m['title'].replace('"','')
-        keywords = m['keywords']
-        SQL = SQL_INSERT.strip().replace('\n','')
-        db.executescript(SQL % (bibcode,name,start,duration,keywords,bibcode,name,lat,lng))
+        print "Geocode returned lat,lng [%s,%s] for [%s]" % (lat,lng,L)
+        if strategy == "insert":
+          SQL = SQL_INSERT.strip().replace('\n','')
+          SQL = SQL % (bibcode,name,start,duration,keywords,bibcode,name,lat,lng)
+        elif strategy == "update":
+          SQL = SQL_UPDATE.strip().replace('\n','')
+          SQL = SQL & (lat,lng,name,bibcode)
+        db.executescript(SQL)
         db.commit()
     y+=1
 
